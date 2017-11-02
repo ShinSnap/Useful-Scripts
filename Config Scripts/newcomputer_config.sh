6f5ecc53c8d92b337938e9838dd4a5160e1f1448
Phillip Pansoy <xshijimax@gmail.com>
2017-11-01 19:37:09 -0700

    Comment/uncommented some installs. Added discord, steam, royal-tsx, rvm, and gitkraken

--- Config Scripts/newcomputer_config.sh
+++ Config Scripts/newcomputer_config.sh
@@ -4,6 +4,7 @@
 ############################################
 ## Script For New Computer or Fresh Image ##
 ########### by C3R34LK1LL3R ################
+######## Modified by ShinSnap ##############
 ############################################
 
 echo Install all AppStore Apps at first!
@@ -28,8 +29,8 @@ Brew install cask
 
 # Core Functionality
 echo Install Core Apps
-brew cask install --appdir="/Applications" dropbox
-brew cask install --appdir="~/Applications" vlc
+#brew cask install --appdir="/Applications" dropbox
+#brew cask install --appdir="~/Applications" vlc
 brew cask install --appdir="~/Applications" iterm2
 brew cask install --appdir="~/Applications" java
 
@@ -37,17 +38,19 @@ brew cask install --appdir="~/Applications" java
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 ## get from App Store
-#brew cask install --appdir="/Applications" evernote
+brew cask install --appdir="/Applications" evernote
 #brew cask install --appdir="/Applications" wunderlist
 #brew cask install --appdir="/Applications" clamxav
 
 # Development
 echo Installing Dev Apps
-#brew cask install --appdir="/Applications" github
+brew cask install --appdir="/Applications" github
+brew cask install --appdir="/Applications" gitkraken
+brew cask install --appdir="/Applications" atom
 #brew cask install --appdir="/Applications" heroku-toolbelt
-brew cask install --appdir="/Applications" sublime-text
+#brew cask install --appdir="/Applications" sublime-text
 #brew cask install --appdir="/Applications" webstorm
-brew cask install --appdir="/Applications" pycharm-ce
+#brew cask install --appdir="/Applications" pycharm-ce
 #brew cask install --appdir="/Applications" light-table
 #brew cask install --appdir="/Applications" macvim
 brew cask install --appdir="/Applications" virtualbox
@@ -55,6 +58,10 @@ brew cask install --appdir="/Applications" vagrant
 #brew cask install --appdir="/Applications" sourcetree
 #brew cask install --appdir="/Applications" charles
 #brew cask install --appdir="/Applications" easyfind
+
+# Ruby Environment Manager
+echo Installing RVM
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 
 # Google Slavery
@@ -67,7 +74,7 @@ brew cask install --appdir="/Applications" google-chrome
 
 # Nice to have
 echo Install Some additional Apps
-brew cask install --appdir="/Applications" firefox
+#brew cask install --appdir="/Applications" firefox
 #brew cask install --appdir="/Applications" skype
 #brew cask install --appdir="/Applications" jdownloader
 #brew cask install --appdir="/Applications" lastfm
@@ -75,10 +82,12 @@ brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" spotify-notifications
 brew cask install --appdir="/Applications" snagit
+brew cask install --appdir="/Applications" discord
+brew cask install --appdir="/Applications" steam
+brew cask install --appdir="/Applications" royal-tsx
 
 
 # cleanup
 echo Cleaning up installation
 brew cleanup --force
 rm -f -r /Library/Caches/Homebrew/*
-
